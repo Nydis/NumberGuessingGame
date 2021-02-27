f51b3f2c6ae43537c9041d2994109b55934901f7
Nydis <59804636+Nydis@users.noreply.github.com>
2021-02-27 23:52:42 +0100

    randomNumber() insert() correctnessTest() added

--- src/app.java
+++ src/app.java
@@ -1,25 +1,44 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class app {
 
     public static void main(String[] args) {
         greeting();
-
+        randomNumber();
+        insert();
+        correctnessTest(int rand,int insertedNumber);
     }
 
     static void greeting() {
         System.out.println("Welcome to the NumberGuessingGame!");
-        System.out.println("The Rules are simple: Enter a Number between 1 and 100. If you guess the correct Number, you will win.");
+        System.out.println("The Rules are simple: Enter a Number between 0 and 100. If you guess the correct Number, you will win.");
     }
 
+    static int randomNumber() {
+        Random random = new Random();
+        int rand = random.nextInt(100);
+        System.out.println(rand);
+        return rand;
+    }
 
+    static int insert() {
+        Scanner scanner = new Scanner(System.in);
+        int insertedNumber = scanner.nextInt();
+        return insertedNumber;
+    }
 
-
-
-
-
-
-
+    static void correctnessTest(int rand, int insertedNumber) {
+        if (insertedNumber == rand) {
+            System.out.println("You Win!");
+        } else if (insertedNumber > rand) {
+            System.out.println("Inserted Number is too high, please try again!");
+        } else if (insertedNumber < rand) {
+            System.out.println("Inserted Number is too low, please try again!");
+        } else {
+            System.out.println("Wrong Input, please insert a Number from 0 - 100");
+        }
+    }
 
 }
 
