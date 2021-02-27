3c7b5c4f0ab6cc023f7222f3d014a84aec2dbc6e
Nydis <59804636+Nydis@users.noreply.github.com>
2021-02-27 14:30:33 +0100

    greeting method added

--- src/app.java
+++ src/app.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class app {
 
     public static void main(String[] args) {
-        System.out.println("Hello Again!");
-
+        greeting();
 
     }
+
+    static void greeting() {
+        System.out.println("Welcome to the NumberGuessingGame!");
+        System.out.println("The Rules are simple: Enter a Number between 1 and 100. If you guess the correct Number, you will win.");
+    }
+
+
+
+
+
+
+
+
+
+
 }
+
